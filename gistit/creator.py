00f0506df62495904b30322045ee66793a5ff118
Shubham Chaudhary <me@shubhamchaudhary.in>
2015-01-29 12:13:07 +0530

    Created main logic code

---
+++ gistit/creator.py
@@ -0,0 +1,51 @@
+import sys
+import time
+
+if sys.version_info >= (3,):
+    import urllib.request as urllib2
+    import urllib.parse as urlparse
+    import urllib.error as urlerror
+else:
+    import urllib2
+    import urlparse
+
+
+class Creator:
+    ''' Gist Creator class '''
+    def create(self, content, public=True, filename=None):
+        ''' Creates a gist with the following content '''
+        url = 'https://api.github.com/gists'
+        if not filename:
+            filename = 'gistit' + time.strftime("_%Y-%m-%d_%H%M")   # "%d/%m/%Y"
+        values = {
+                'public' : public,
+                'description' : 'Created using gistit from PyPi',
+                'files' : {
+                    filename: {
+                        'content': content
+                        }
+                    }
+                }
+        # Create request
+        data = urlparse.urlencode(values)
+        data = data.encode('utf-8')
+        req = urllib2.Request(url, data)
+        # Send request
+        try:
+            response = urllib2.urlopen(req)         #res.geturl(), .url=str, .status=200, .info=200, .msg=OK,
+        except urlerror.HTTPError as exep:
+            print('The server couldn\'t fulfill the request.',
+                  'Error code: ', exep.code)
+            raise
+        except urlerror.URLError as exep:
+            print('We failed to reach a server.')
+            print('Reason: ', exep.reason)
+            raise
+        except:
+            raise
+        else:
+            # everything is fine
+            the_page = str(response.read()) # More pythonic than .decode('utf-8')
+            # Parse for success or failure
+            print(the_page)
+
---
+++ gistit/gistit.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+
+import atexit
+import argparse
+import os
+import platform
+import sys
+
+import creator
+
+def stop_for_windows():
+    ''' If on windows platform, stop and wait for input
+    '''
+    if os.name == 'nt' or platform.system() == 'Windows':
+        input('Press Enter or Close the window to exit !')
+
+def main():
+    ''' Main Function '''
+    # Run this function everytime on exit
+    atexit.register(stop_for_windows)
+    # Parse command line arguments
+    #usage = "%prog [-f credential_file]"
+    #parser = ArgumentParser(usage=usage)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "--file", type=str, dest="file",
+            help="Use the specified file")
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument("-r", "--private", required=False, action='store_true',
+            dest="private", help='Make the gist private')
+    group.add_argument("-u", "--public", required=False, action='store_true',
+            dest="public", help='Make the gist public (DEFAULT)')
+    #parser.add_argument('otherthings', nargs='*')
+    #args = parser.parse_args()
+    args, otherthings = parser.parse_known_args()
+
+    if args.file:
+        filename = args.file
+    elif len(otherthings) > 0:
+        filename = otherthings[0]
+    else:
+        print('FATAL: Filename not specified')
+        return -1
+    # Check that file conforms size limit of 1MB
+    statinfo = os.stat(filename)
+    if statinfo.st_size < 1000000:
+        fhan = open(filename, 'rU')
+        file_content = fhan.read()
+    else:
+        print('ERROR: File size exceeds specified limit of 1MB')
+        return -1
+    public = True   #args.public
+    if args.private:
+        public = False
+    creator_obj = creator.Creator()
+    creator_obj.create(file_content, public=public, filename=filename)
+
+if __name__ == '__main__':
+    try:
+        return_code = main()
+        sys.exit(return_code)
+    except KeyboardInterrupt:
+        print('\nClosing garacefully :)', sys.exc_info()[1])
+    #TODO: Handle other errors
+    except SystemExit:
+        pass
+    except:
+        print('Unexpected Error:', sys.exc_info()[0])
+        print('Details:', sys.exc_info()[1])
+        raise
+
