67204367923929659d8c332538010d4243437b56
Shubham Chaudhary <me@shubhamchaudhary.in>
2015-02-02 16:39:49 +0530

    Update version number and close files properly

--- setup.py
+++ setup.py
@@ -7,19 +7,23 @@ add_keywords = dict(
 )
 
 fhan = open('requirements.txt', 'rU')
-requires = [ line.strip() for line in fhan.readlines() ]
+requires = [line.strip() for line in fhan.readlines()]
+fhan.close()
 #print('We require: ', requires)
+fhan = open('README.txt')
+long_description = fhan.read()
+fhan.close()
 
 setup(
         name='GistIt',
         description='GistIt helps create github gists quickly',
-        version='0.0.1',
+        version='0.0.2',
         packages=['gistit'],
         license='GPLv3+',
         author='Shubham Chaudhary',
         author_email='me@shubhamchaudhary.in',
         url='https://github.com/shubhamchaudhary/gistit',
-        long_description=open('README.txt').read(),
+        long_description=long_description,
         install_requires=requires,
         **add_keywords
 )
