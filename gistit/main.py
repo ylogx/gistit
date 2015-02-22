9ac464c7b46a576756123966e79aa56fb0f599ff
Shubham Chaudhary <me@shubhamchaudhary.in>
2015-02-22 06:23:40 +0530

    Code cleanup

--- gistit/main.py
+++ gistit/main.py
@@ -16,21 +16,15 @@ def stop_for_windows():
 
 def main():
     ''' Main Function '''
-    # Run this function everytime on exit
     atexit.register(stop_for_windows)
-    # Parse command line arguments
-    #usage = "%prog [-f credential_file]"
-    #parser = ArgumentParser(usage=usage)
     parser = argparse.ArgumentParser()
     parser.add_argument("-f", "--file", type=str, dest="file",
-            help="Use the specified file")
+                        help="Use the specified file")
     group = parser.add_mutually_exclusive_group()
     group.add_argument("-r", "--private", required=False, action='store_true',
-            dest="private", help='Make the gist private')
+                       dest="private", help='Make the gist private')
     group.add_argument("-u", "--public", required=False, action='store_true',
-            dest="public", help='Make the gist public (DEFAULT)')
-    #parser.add_argument('otherthings', nargs='*')
-    #args = parser.parse_args()
+                       dest="public", help='Make the gist public (DEFAULT)')
     args, otherthings = parser.parse_known_args()
 
     content_to_post = ''
@@ -76,7 +70,6 @@ if __name__ == '__main__':
         sys.exit(return_code)
     except KeyboardInterrupt:
         print('\nClosing garacefully :)', sys.exc_info()[1])
-    #TODO: Handle other errors
     except SystemExit:
         pass
     except:
