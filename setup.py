d5992b7964f412c35e2f3c05b54fb7d4c22327f1
Shubham Chaudhary <me@shubhamchaudhary.in>
2015-05-13 12:53:02 +0530

    Use pandoc to convert md to rst

--- setup.py
+++ setup.py
@@ -9,10 +9,14 @@ add_keywords = dict(
 fhan = open('requirements.txt', 'rU')
 requires = [line.strip() for line in fhan.readlines()]
 fhan.close()
-#print('We require: ', requires)
-fhan = open('README.txt')
-long_description = fhan.read()
-fhan.close()
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except (IOError, ImportError):
+    fhan = open('README.txt')
+    long_description = fhan.read()
+    fhan.close()
+
 
 setup(
         name='GistIt',
